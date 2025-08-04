1dad6bdcb9c3b241c796a669deddd9d9c1261ae5
Guilherme Dourado <guiihdourado@gmail.com>
2025-08-04 11:17:10 -0300

    fazendo a a rota de test de consultas

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -1,4 +1,4 @@
-import {expect, test, beforeAll, afterAll, describe} from 'vitest';
+import { expect, test, beforeAll, afterAll, describe } from 'vitest';
 import server from 'node:http'
 import request from 'supertest'
 import { app } from '../src/app'
@@ -7,18 +7,29 @@ describe('transactions routes', () => {
     beforeAll(async () => {
         await app.ready()
     })
-    
+
     afterAll(async () => {
         await app.close()
     })
-    
-    test('o usuario consegue criar uma nova transicao', async () => {
-        const response = await request(app.server).post('/transactions').send({
+
+    test.only('o usuario consegue criar uma nova transicao no test', async () => {
+        await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'new transaction',
+                amount: 5000,
+                type: 'credit'
+            })
+            .expect(201)
+    })
+
+    test.skip('o usuario consegue consultar todas transicao no test', async () => {
+        const response = await request(app.server)
+        .post('/transactions')
+        .send({
             title: 'new transaction',
             amount: 5000,
             type: 'credit'
         })
-        .expect
     })
-
 })
