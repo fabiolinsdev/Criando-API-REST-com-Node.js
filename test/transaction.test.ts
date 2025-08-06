c6d99bd49d4ee7522bc49b4bfacbfd481ac6a168
Fabio Lins <binhobsb_fabio29@outlook.com>
2025-08-06 13:19:57 -0300

    testando listagem de transaction

--- test/transaction.test.ts
+++ test/transaction.test.ts
@@ -2,6 +2,7 @@ import { expect, test, beforeAll, afterAll, describe } from 'vitest';
 import server from 'node:http'
 import request from 'supertest'
 import { app } from '../src/app'
+import { setCookie } from 'undici-types';
 
 describe('transactions routes', () => {
     beforeAll(async () => {
@@ -12,7 +13,7 @@ describe('transactions routes', () => {
         await app.close()
     })
 
-    test.only('o usuario consegue criar uma nova transicao no test', async () => {
+    test('o usuario consegue criar uma nova transicao no test', async () => {
         await request(app.server)
             .post('/transactions')
             .send({
@@ -23,13 +24,27 @@ describe('transactions routes', () => {
             .expect(201)
     })
 
-    test.skip('o usuario consegue consultar todas transicao no test', async () => {
-        const response = await request(app.server)
+    test('o usuario consegue consultar todas transicao no test', async () => {
+        const createTransactionResponse = await request(app.server)
         .post('/transactions')
         .send({
             title: 'new transaction',
             amount: 5000,
             type: 'credit'
         })
+
+        const cookie = createTransactionResponse.get('setCookie')
+
+        const listTransactionsResponse = await request(app.server)
+            get('/transactions')
+            set('Cookie', cookies)
+            expect(200)
+        
+        expect(listTransactionsResponse.body.transactions).toEqual([
+            expect.objectContaining({
+                title: 'New transaction',
+                amount: 5000,
+            }),
+        ])    
     })
 })
